59035f48f1bbb7c61ad298077b953709e4646226
山本周 <yamamotoshu@yamamotoamanenoMacBook-Air.local>
2020-03-16 16:06:46 +0900

    finish task1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
 
   # GET /books/1/edit
   def edit
+    @book = Book.find(params[:id])
   end
 
   # POST /books
@@ -24,6 +25,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to @book, notice: 'Book was successfully created.'
     else
+      @books = Book.all
       render :index
     end
   end
@@ -31,11 +33,9 @@ class BooksController < ApplicationController
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    if @book.update()
-      redirect_to @book, notice: 'Book was successfully updated.'
-    else
-      render :edit
-    end
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to @book, notice: 'Book was successfully updated.'
   end
 
   # DELETE /books/1
